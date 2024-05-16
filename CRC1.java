44123e1b7f5aabc70759cfe7b6cc19020eeabb15
Prathameshh13 <139326857+Prathameshh13@users.noreply.github.com>
2024-05-16 21:41:39 +0530

    Add files via upload

--- CRC1.java
+++ CRC1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class CRC1{
-    
+public class CRC1 {
+
     public static String xor(String a, String b) {
         StringBuilder result = new StringBuilder();
         for (int i = 1; i < b.length(); i++) {
@@ -18,7 +18,7 @@ public class CRC1{
         int pick = divisor.length();
         String tmp = dividend.substring(0, pick);
         int n = dividend.length();
-        
+
         while (pick < n) {
             if (tmp.charAt(0) == '1') {
                 tmp = xor(divisor, tmp) + dividend.charAt(pick);
@@ -33,7 +33,7 @@ public class CRC1{
         } else {
             tmp = xor("0".repeat(pick), tmp);
         }
-        
+
         return tmp;
     }
 
@@ -48,23 +48,33 @@ public class CRC1{
         return mod2div(codeword, key);
     }
 
+    public static int findErrorPosition(String codeword, String key) {
+        String remainder = mod2div(codeword, key);
+        int errorPos = 0;
+
+        for (int i = 0; i < remainder.length(); i++) {
+            if (remainder.charAt(i) == '1') {
+                errorPos += (int) Math.pow(2, remainder.length() - 1 - i);
+            }
+        }
+        return errorPos;
+    }
+
+    public static String correctError(String codeword, int errorPos) {
+        if (errorPos > 0 && errorPos <= codeword.length()) {
+            StringBuilder correctedCodeword = new StringBuilder(codeword);
+            correctedCodeword.setCharAt(errorPos - 1, (codeword.charAt(errorPos - 1) == '0') ? '1' : '0');
+            return correctedCodeword.toString();
+        }
+        return codeword;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Enter a binary string of 7 or 8 bits: ");
         String data = scanner.nextLine();
 
-        // Validate input length
-        if (data.length() != 7 && data.length() != 8) {
-            System.out.println("Invalid input length. Please enter a binary string of exactly 7 or 8 bits.");
-            return;
-        }
-
-        // Validate input content
-        if (!data.matches("[01]+")) {
-            System.out.println("Invalid input content. Please enter a binary string containing only '0' and '1'.");
-            return;
-        }
 
         String key = "100000111";  // CRC-8 polynomial
 
@@ -77,22 +87,17 @@ public class CRC1{
         System.out.print("Enter corrupted binary string: ");
         String corruptedData = scanner.nextLine();
 
-        // Validate corrupted data length
-        if (corruptedData.length() != encodedData.length()) {
-            System.out.println("Invalid corrupted data length. Please enter a binary string of length: " + encodedData.length());
-            return;
-        }
-
-        // Validate corrupted data content
-        if (!corruptedData.matches("[01]+")) {
-            System.out.println("Invalid corrupted data content. Please enter a binary string containing only '0' and '1'.");
-            return;
-        }
-
+      
         System.out.println("\nDecoding...");
         String remainder = decodeData(corruptedData, key);
         if (remainder.contains("1")) {
             System.out.println("Error detected in received data.");
+            int errorPos = findErrorPosition(corruptedData, key);
+            System.out.println("Error detected at bit position: " + errorPos);
+            String correctedData = correctError(corruptedData, errorPos);
+            System.out.println("Corrected binary data: " + correctedData);
+            String decodedBinaryData = correctedData.substring(0, correctedData.length() - key.length() + 1);
+            System.out.println("Decoded binary data: " + decodedBinaryData);
         } else {
             System.out.println("No error detected in received data.");
             String decodedBinaryData = corruptedData.substring(0, corruptedData.length() - key.length() + 1);
